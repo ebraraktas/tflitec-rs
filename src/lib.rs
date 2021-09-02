9a8db86fdc9efdba679a4377f6642dccda17852f
ebraraktas <ebrar@voys.ai>
2021-09-02 16:32:08 +0300

    Create bindings module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 pub mod interpreter;
 pub mod model;
 pub mod tensor;
+
+pub(crate) mod bindings {
+    #![allow(clippy::all)]
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+    #![allow(unused)]
+    #![allow(improper_ctypes)]
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
